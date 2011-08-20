a698c075f4f214cb87defc771a436d5ed6f8767f
Kevin Solorio <ksolorio@within3.com>
2011-08-20 14:14:32 -0400

    uncommented the requirement for sqlite3

--- config/environment.rb
+++ config/environment.rb
@@ -17,8 +17,8 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem "authlogic", :version => '2.1.5'
   config.gem "paperclip", :version => '2.3.3'
   config.gem "activemerchant", :lib => "active_merchant", :version => "1.7.1"
