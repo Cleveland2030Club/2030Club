850c94308a9e3b4512183279848b2db610b6e60a
Kevin Solorio <ksolorio@within3.com>
2011-09-05 21:30:42 -0400

    hotfix: display event for gautam before normal time
    We are getting paid to promote this event and was
    asked to elevate this to one item to the front page
    for a longer duration of time.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    @special_event = Event.find(232)
     @events = Event.coming_soon
     @sponsored_events = @events.with_sponsor
     @unsponsored_events = @events - @sponsored_events
