f9fc144ef0bf8d1dca8defe5d7f658bd5038aabc
Kevin Solorio <ksolorio@within3.com>
2011-09-17 16:17:25 -0400

    added Participant validations

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,4 +2,5 @@ class Participant < ActiveRecord::Base
   
   has_attached_file :logo, :styles => { :medium => "350x250>", :thumb => "175x125>" }
   
+  validates_presence_of :name, :bio, :benefit, :instructions
 end
--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -11,4 +11,30 @@ describe Participant do
     it { should respond_to(:logo) }
   end
 
+  describe "a valid Participant requires" do
+    before do
+      @participant = Participant.new(:name => "Company", :url => "http://company.com",
+        :bio => "Hello World", :benefit => "Discount goes here", :instructions => "How to redeem")
+    end
+    
+    it "a name" do
+      @participant.name = ""
+      @participant.should_not be_valid
+    end
+    
+    it "a bio" do
+      @participant.bio = ""
+      @participant.should_not be_valid
+    end
+    
+    it "a benefit" do
+      @participant.benefit = ""
+      @participant.should_not be_valid
+    end
+    
+    it "instructions" do
+      @participant.instructions = ""
+      @participant.should_not be_valid
+    end
+  end
 end
