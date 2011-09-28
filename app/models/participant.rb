d451e45b94a1f39a6d3bc808e1c90ecfc146b6e9
Kevin Solorio <ksolorio@within3.com>
2011-09-27 21:29:47 -0400

    reorganize participant class
    relationships, validations, attachment

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,8 +1,8 @@
 class Participant < ActiveRecord::Base
   
-  has_attached_file :logo, :styles => { :medium => "350x250>", :thumb => "175x125>" }
-  
   belongs_to :category
   
   validates_presence_of :name, :bio, :benefit, :instructions
+  
+  has_attached_file :logo, :styles => { :medium => "350x250>", :thumb => "175x125>" }
 end
