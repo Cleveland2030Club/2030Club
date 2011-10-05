3138b1b79499d274e653f87606396e374f8a3314
Kevin Solorio <ksolorio@within3.com>
2011-10-05 15:01:21 -0400

    remove special event from home page and controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    @special_event = Event.find(232)
     @events = Event.coming_soon
     @sponsored_events = @events.with_sponsor
     @unsponsored_events = @events - @sponsored_events
