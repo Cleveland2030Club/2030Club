59a69b66efdf8a560e393f54473ddabe96be6940
Jonathan Knapp <jon@coffeeandcode.com>
2012-03-24 13:05:00 -0400

    Removed admins who have resigned

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ActiveRecord::Base
   has_many  :users_roles
   has_many  :roles, :through => :users_roles
   has_one   :user_profile, :dependent => :destroy
-  accepts_nested_attributes_for :user_profile 
+  accepts_nested_attributes_for :user_profile
   has_one   :registration, :dependent => :destroy
   has_one   :user_address, :dependent => :destroy
   has_many  :orders, :as => :customer
 
-  has_attached_file :avatar, 
+  has_attached_file :avatar,
                     :styles => { :medium => "100x100>", :thumb => "50x50" }
 
   acts_as_authentic do |c|
@@ -53,25 +53,35 @@ class User < ActiveRecord::Base
     self.expired_at = DateTime.new(activated_at.year + 1, activated_at.month, -1)
     self.save
   end
-  
+
   def admin?
-    admins = ['jon@coffeeandcode.com', 'morcutt@within3.com', 'kevin@detone8.com', 'agbennett1@gmail.com', 'jprovins@cleveland.com', 'julie.fratus@gmail.com', 'eljeezee@yahoo.com', 'gautpai@gmail.com', 'radinm@hotmail.com', 'montalvo.a@gmail.com', 'christinannmiller@yahoo.com', 'raablc@gmail.com', 'jessicadaiseysnyder@yahoo.com' ]
+    admins = ['jon@coffeeandcode.com',
+              'morcutt@within3.com',
+              'kevin@detone8.com',
+              'agbennett1@gmail.com',
+              'eljeezee@yahoo.com',
+              'gautpai@gmail.com',
+              'radinm@hotmail.com',
+              'montalvo.a@gmail.com',
+              'christinannmiller@yahoo.com',
+              'raablc@gmail.com'
+              ]
     admins.include?(self.email)
   end
-  
+
   def deliver_password_reset_instructions!
     reset_perishable_token!
     UserMailer.deliver_password_reset_instructions(self)
   end
-  
+
   def attendance
       user_orders = self.orders.collect do |order|
         order.events
       end
       user_orders.flatten
-    
+
   end
-  
+
   def self.search_active(search,date,page)
     unless date == "-" or date == nil
       paginate_all_by_active true, :per_page => 100, :page => page,
@@ -81,7 +91,7 @@ class User < ActiveRecord::Base
                :conditions => ['lower(last_name) like lower(?) or lower(first_name) like lower(?) or lower(email) like lower(?)', "%#{search}%","%#{search}%","%#{search}%"], :order => 'Last_name ASC, first_name ASC, email ASC'
     end
   end
-  
+
   def self.search(search,date,page)
     unless date == "-" or date == nil
       paginate :per_page => 100, :page => page,
@@ -101,7 +111,7 @@ class User < ActiveRecord::Base
                :conditions => ['lower(last_name) like lower(?) or lower(first_name) like lower(?) or lower(email) like lower(?)', "%#{search}%","%#{search}%","%#{search}%"], :order => 'Last_name ASC, first_name ASC, email ASC'
     end
   end
-  
+
   def self.csv_search(search,date)
     unless date == "-" or date == nil
       paginate :per_page => 30000, :page => 1,
