e08397ba100404acbc4032d63e03a9d08db9d7ff
kmsolorio <kevin@detone8.com>
2012-03-25 14:43:31 -0400

    add mailer and update controller for membership order flows

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -30,4 +30,15 @@ class UserMailer < ActionMailer::Base
     body        :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
   end
 
+  def renewal_email(user)
+    events = Event.all(:conditions => ["start_at > DATE(?)", Time.now], :order => "start_at ASC", :limit => 5)
+    
+    recipients  user.email
+    bcc         "technology@cleveland2030.org, info@cleveland2030.org"
+    from        "Cleveland2030 Club <do_not_reply@cleveland2030.org>"
+    subject     "Membership Renewal"
+    sent_on     Time.now
+    body        :user => user, :url => "http://cleveland2030.org/login", :events => events
+  end
+
 end
