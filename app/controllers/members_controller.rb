660f510cf8f819bc44dc57b8aa5c0e97eaab420d
Kevin Solorio <kevin@detone8.com>
2012-06-19 23:42:12 -0400

    begin cleanup member profiles

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,5 @@
 class MembersController < ApplicationController
+
   def index
     @members = User.search_active(params[:search],@date,params[:page])
 	end	
@@ -8,35 +9,15 @@ class MembersController < ApplicationController
     @events = @member.attendance
   end
   
-  def edit
-    if current_user.id == params[:id].to_i
-    	@member = current_user
-		elsif current_user.admin?
-		  @member = User.find(params[:id])
-		else
-			redirect_to new_user_session_path
-  	end
-  end
-  
-  def update
-    if current_user.id == params[:id].to_i
-      @member = current_user
-    elsif current_user.admin?
-      @member = User.find(params[:id])
-    end
-    if @member.update_attributes(params[:user])
-      redirect_to member_url(@member)
-    else
-      render :action => :edit
-    end
-  end
-  
-  def edit_login
-    edit
-  end
-
   def search
     @members = User.all_members
 	end
+
+  private
+
+  def set_member
+    return @member = current_user if current_user.id == params[:id].to_i
+    return @member = User.find(params[:id]) if current_user.admin?
+  end
   
 end
