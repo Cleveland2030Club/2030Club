b3b7c9e51fdbef45c2fd071d04ae6848f82ae506
Kevin Solorio <kevin@detone8.com>
2012-06-21 00:23:03 -0400

    restrict edit and update actions to owner of account

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
 
   before_filter :require_user
+  before_filter :verify_owner, :only => [:edit, :create]
 
   def new
     @profile = UserProfile.new(params[:user_profile])
@@ -28,6 +29,14 @@ class ProfilesController < ApplicationController
       redirect_to user_path
     else
       render :edit
+    end
+  end
+
+  private
+
+  def verify_owner
+    unless current_user.user_profile.id == params[:id].to_i
+      redirect_to new_user_sessions_path
     end
   end
 
