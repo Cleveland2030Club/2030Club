ca4efbd4dd9d5fabaa7ee81fe2fd39b493079cba
Kevin Solorio <kevin@detone8.com>
2012-06-29 23:31:35 -0400

    update application.rb file

--- config/application.rb
+++ config/application.rb
@@ -1,65 +1,68 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Cle2030
   class Application < Rails::Application
-    config.autoload_paths += [config.root.join('lib')]
-    config.encoding = 'utf-8'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  
-    # Add additional load paths for your own custom dirs
-    # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  
-    # Specify gems that this application depends on and have them installed with rake gems:install
-    # config.gem "bj"
-    # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-    # config.gem "aws-s3", :lib => "aws/s3"
-    # config.gem "sqlite3-ruby", :lib => "sqlite3"
-    config.gem "authlogic", :version => '2.1.5'
-    config.gem "paperclip", :version => '2.3.3'
-    config.gem "activemerchant", :lib => "active_merchant", :version => "1.7.1"
-    config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
-    config.gem 'RedCloth'
-  
+
+    # Custom directories with classes and modules you want to be autoloadable.
+    # config.autoload_paths += %W(#{config.root}/extras)
+
     # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named
+    # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  
-    #config.plugins = [:event_calendar]
-  
-    # Skip frameworks you're not going to use. To use Rails without a database,
-    # you must remove the Active Record framework.
-    # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-  
-    # Activate observers that should always be running
+
+    # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-  
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names.
-    config.time_zone = 'Eastern Time (US & Canada)'
-  
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  
-    # support yaml and heroku config vars, preferring ENV for heroku
-    SETTINGS = (YAML.load_file('config/cleveland_2030_settings.yml')[RAILS_ENV] rescue {}).merge(ENV)
-  
-    # ActionMailer settings
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      :enable_starttls_auto => true,
-      :address => 'smtp.gmail.com',
-      :port => 587,
-      :authentication => :plain,
-      :domain => 'cleveland2030.org',
-      :user_name => SETTINGS['USER_NAME'],
-      :password => SETTINGS['USER_PW']
-    }
+
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
+
+    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    # config.active_record.schema_format = :sql
+
+    # Enforce whitelist mode for mass assignment.
+    # This will create an empty whitelist of attributes available for mass-assignment for all models
+    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
+    # parameters by using an attr_accessible or attr_protected declaration.
+    config.active_record.whitelist_attributes = true
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
