8f26c25af2ec1231a51e653da50d87ad8f831c63
Kevin Solorio <kevin@detone8.com>
2012-06-29 23:41:55 -0400

    remaining rails 3 files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cle2030::Application.config.secret_token = '2d292fd8c98e1cb8297d2799a44e922e5c8a45464f74002df6be0568bb5b7028ec03a587bdfa27173549c19f81effafe0d59c5474ea3d26dcd49cfed5e8cbdf4'
