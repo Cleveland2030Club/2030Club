d6d921137183dd5bd34856af25b63f2ac017ac9e
Matthew Case <villadweller@gmail.com>
2012-07-07 12:42:45 -0400

    Modifying instances of named_scope to be 'scope'. Fixes #17

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,5 @@
 class Membership < ActiveRecord::Base
   has_many :items,  :as => :product
 
-  named_scope :standard, :conditions => {:name => 'Standard'}
+  scope :standard, :conditions => {:name => 'Standard'}
 end
