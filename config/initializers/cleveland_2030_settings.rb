5389835e83f40c6ec5775a256f3fc34e96ead418
Kevin Solorio <kevin@detone8.com>
2012-08-03 00:27:02 -0400

    updated config
    Rails2 used RAILS_ENV and RAILS_ROOT, updated to
    Rails.env and Rails.root

--- config/initializers/cleveland_2030_settings.rb
+++ config/initializers/cleveland_2030_settings.rb
@@ -3,7 +3,7 @@ require 'yaml'
 module Cleveland2030Settings
   def self.config
     if not defined? @config
-      @config = (YAML.load_file("#{RAILS_ROOT}/config/cleveland_2030_settings.yml")[RAILS_ENV] rescue {}).merge(ENV)
+      @config = (YAML.load_file("#{Rails.root}/config/cleveland_2030_settings.yml")[Rails.env] rescue {}).merge(ENV)
     end
     @config
   end
