0723e5cb509205f7a23aaed7bce6616ba0f96a18
Kevin Solorio <kevin@detone8.com>
2012-09-06 21:49:54 -0400

    add attr_accessible for guest model

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,10 @@
 class Guest < ActiveRecord::Base
+
+  attr_accessible :first_name, :last_name, :email
   
   validates_presence_of :first_name, :last_name, :email
   validates_format_of :email,
     :with => RegistrationsHelper::EMAIL_REGEX,
     :if => :email?
+    
 end
