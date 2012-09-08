c8907cbb8965ea990d1058dbbebc074ead41e209
Kevin Solorio <kevin@detone8.com>
2012-09-08 15:34:09 -0400

    add attr_accessible for location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,7 @@
 class Location < ActiveRecord::Base
 
+  attr_accessible :region_id, :address, :address_cont, :city, :zip, :google_map_link, :phone
+
   belongs_to :participant
   belongs_to :region
 
