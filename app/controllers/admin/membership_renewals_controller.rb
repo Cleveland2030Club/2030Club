055c861b8ce7f2bcec31764dba88c7199af34ad0
Kevin Solorio <kevin@detone8.com>
2013-03-15 23:11:38 -0400

    display basic member info to get renewal done

--- app/controllers/admin/membership_renewals_controller.rb
+++ app/controllers/admin/membership_renewals_controller.rb
@@ -1,5 +1,21 @@
 module Admin
   class MembershipRenewalsController < Admin::BaseController
 
+    def create
+      @user = User.find(params[:user_id])
+      @user.update_membership_expiration(renew_date)
+      redirect_to admin_user_path(@user)
+    end
+
+    protected
+
+    def renew_date
+      if params[:renew_date].blank?
+        Time.now
+      else
+        params[:renew_date]
+      end
+    end
+
   end
 end
