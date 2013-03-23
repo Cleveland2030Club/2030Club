d4e09d5a66ab24df11e209e35d5d0006b8542db9
Kevin Solorio <kevin@detone8.com>
2013-03-22 23:17:44 -0400

    outline controller actions

--- app/controllers/admin/board_terms_controller.rb
+++ app/controllers/admin/board_terms_controller.rb
@@ -1,4 +1,30 @@
 module Admin
   class BoardTermsController < Admin::BaseController
+
+    before_filter :set_user
+
+    def new
+      @board_term = @user.board_terms.new(params[:board_term])
+    end
+
+    def edit
+      @board_term = @user.board_terms.find(params[:id])
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    protected
+
+    def set_user
+      @user = User.includes(:board_terms).find(params[:user_id])
+    end
+
   end
 end
