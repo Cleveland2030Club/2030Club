d4217f127896721393729acb72427d19be7a2e22
Kevin Solorio <kevin@detone8.com>
2013-03-22 22:37:36 -0400

    update default ordering on board terms

--- app/models/board_term.rb
+++ app/models/board_term.rb
@@ -2,5 +2,5 @@ class BoardTerm < ActiveRecord::Base
   belongs_to :user
   attr_accessible :end_at, :position, :start_at, :user_id
 
-  default_scope order('position ASC')
+  default_scope order('position ASC, end_at DESC')
 end
