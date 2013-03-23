7258f9f931cf56e3c61c4dca9d9f55657556c75c
Kevin Solorio <kevin@detone8.com>
2013-03-22 22:37:52 -0400

    add BoardPosition Model

---
+++ app/models/board_position.rb
@@ -0,0 +1,33 @@
+class BoardPosition
+
+  TITLES = {
+    1 => "President",
+    2 => "President Elect",
+    3 => "Secretary",
+    4 => "Membership Director",
+    5 => "Education Director",
+    6 => "Networking Director",
+    7 => "Philanthropy Director",
+    8 => "Social Director",
+    9 => "Communications Director",
+    10 => "Technology Director",
+    11 => "Tresurer",
+    12 => "General Counsel"
+  }
+
+  EMAILS = {
+    1 => "president@cleveland2030.org",
+    2 => "presidentelect@cleveland2030.org",
+    3 => "secretary@cleveland2030.org",
+    4 => "membership@cleveland2030.org",
+    5 => "education@cleveland2030.org",
+    6 => "networking@cleveland2030.org",
+    7 => "philanthropy@cleveland2030.org",
+    8 => "social@cleveland2030.org",
+    9 => "communications@cleveland2030.org",
+    10 => "technology@cleveland2030.org",
+    11 => "tresurer@cleveland2030.org",
+    12 => "generalcounsel@cleveland2030.org"
+  }
+
+end
