bff0e9ae4a6791fc2228e56364b55b9ed8206fe3
Jonathan Knapp <jon@coffeeandcode.com>
2013-05-11 18:46:23 -0500

    Added admin_dashboard to precomile list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Cle2030::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( admin_dashboard.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
